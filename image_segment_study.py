3be9bb8c0b1769e23c47b924eb063d13233a3c93
Ambrose-Mcduffy <ambrosemcduffy@gmail.com>
2019-05-09 13:41:30 +1000

    Modified the entire structure into a class, for ease of use.

--- image_segment_study.py
+++ image_segment_study.py
@@ -1,33 +1,58 @@
 import cv2 
 import matplotlib.pyplot as plt
 import numpy as np
-# importing in image file
-img = cv2.imread("data/ancientone.jpg")
-img2 = cv2.imread("data/spidey.jpg")
-img2 = cv2.resize(img2,(2624,1240))
-data = np.array([img,img2])
-# changing the color to RGB from BGR
-img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-img2 = cv2.cvtColor(img2, cv2.COLOR_BGR2RGB)
-# number 
-k = 2
-def preprocess(img):
-      x = img.reshape((-1,3))
-      x = np.float32(x)
-      return x
-flatten_img = preprocess(data)
-criteria = (cv2.TermCriteria_EPS+cv2.TERM_CRITERIA_MAX_ITER,10,1.0)
-retval,labels,centers = cv2.kmeans(flatten_img,k,None,criteria,10, cv2.KMEANS_RANDOM_CENTERS)
-centers = np.uint8(centers)
-segmented_data = centers[labels.flatten()]
+import os
 
-# reshaping 
-segmented_data = segmented_data.reshape(data.shape)
-labels_reshape = labels.reshape(2,data.shape[1], data.shape[2])
-plt.imshow(labels_reshape[1])
-def showmask(img,labels_reshape, cluster = 0):
-      mask_image = np.copy(img)
-      mask_image[labels_reshape[0] ==cluster] = [0,0,0]
-      plt.figure(figsize=(10,20))
-      plt.imshow(mask_image, cmap="gray")
-showmask(data[0].squeeze(), labels_reshape,1)
+class Knn_test(object):
+      def __init__(self,path):
+            self.path= path
+      def getImages(self):
+            '''
+            importing in images, and transformaing into rgb instead of BGR
+            '''
+            if os.path.exists(self.path):
+                  img = cv2.imread(self.path)
+                  img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+                  return img
+            else:
+                  print("path none existent")
+                  return None
+      def processImages(self,images):
+            flatten = images.reshape((-1,3))
+            return np.float32(flatten)
+      def knn_train(self,flatten,epoch=10,k=2,episilon=1.0, show = False):
+            '''
+            training the KNN network.. then exporting out the segmented labeled data
+            '''
+            img = self.getImages()
+            # setting the criteria, and training the network
+            criteria = (cv2.TermCriteria_EPS+cv2.TERM_CRITERIA_MAX_ITER,epoch,episilon)
+            # obtaining the labels
+            retval,labels,centers = cv2.kmeans(flatten,k,None,criteria,epoch, cv2.KMEANS_RANDOM_CENTERS)
+            centers = np.uint8(centers)
+            # obtainig the segmented data
+            segmented_data = centers[labels.flatten()]
+            # reshaping the data to be viewed, and used for masking
+            segmented_data = segmented_data.reshape(img.shape)
+            labels_reshape = labels.reshape(img.shape[0], img.shape[1])
+            if show:
+                  plt.imshow(segmented_data, "gray")
+            return segmented_data, labels_reshape
+      def getMask(self,img,labels_reshape, cluster = 0, show=False):
+            '''
+            this function creates a mask.
+            '''
+            mask_image = np.copy(img)
+            mask_image[labels_reshape ==cluster] = [0,0,0]
+            if show:
+                  plt.figure(figsize=(10,20))
+                  plt.imshow(mask_image, cmap="gray")
+            return mask_image
+            
+
+kt = Knn_test("data/before_got.jpg")
+img = kt.getImages()
+flatten_img = kt.processImages(img)
+segmented_data,labels_reshape = kt.knn_train(flatten_img,k=5, show=True)
+mask = kt.getMask(img,labels_reshape,3,True)
+   
